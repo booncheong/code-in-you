5873c3329afe0d5b113ffdab65c65fac26b72e83
Poh Boon Cheong <pohbooncheong@Pohs-MacBook-Pro.local>
2018-06-06 21:47:48 +0800

    6 June
    javascript can be very fun, but very difficult to understand...

---
+++ 01_html_css/bootstrap_starter/index.js
@@ -0,0 +1,22 @@
+var fred = ['fred', 'flinstone', 'fred@gmail.com', 50]
+var 
+
+for (var i = 0; i < fred.length; i++) {
+    if (i == 0)
+        console.log('First name:', fred(i);
+    else if (i == 1),
+        console.log('Last name:', fred[i]);
+    else if (i == 2),
+        console.log('Email:', fred([i]);
+    else
+        console.log('Age:', 'fred([i]);
+    
+    console.log(fred[i]);
+   
+}
+
+console.log("today's date is" + new Date()
+
+
+
+
